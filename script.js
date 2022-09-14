8d8d77ee3c404267260a9e87e786c743f7c15211
Sean Baker <sbaker101@gmail.com>
2022-09-13 20:50:22 -0500

    Formatted code

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 function generateWordArray(userLength, wordDictionary) {
-    let generatedMessage = [];
+  let generatedMessage = [];
 
-    for (let i = 0; i < userLength; i++) {
-      let randomNum = Math.floor(Math.random() * wordDictionary.length);
-      generatedMessage.push(wordDictionary[randomNum]);
-    }
+  for (let i = 0; i < userLength; i++) {
+    let randomNum = Math.floor(Math.random() * wordDictionary.length);
+    generatedMessage.push(wordDictionary[randomNum]);
+  }
 
-    return generatedMessage;
+  return generatedMessage;
 }
 
 function arrayToString(generatedMessage) {
   let arrayString = "";
   for (let i = 0; i < generatedMessage.length; i++) {
     if (i === generatedMessage.length - 1) {
-        arrayString += generatedMessage[i].toLowerCase();
-        arrayString += "!";
-    }
-    else if (i === 0) {
-        arrayString += generatedMessage[i];
-        arrayString += '-';
-    }
-    else {
-        arrayString += generatedMessage[i].toLowerCase();
-        arrayString += "-";
+      arrayString += generatedMessage[i].toLowerCase();
+      arrayString += "!";
+    } else if (i === 0) {
+      arrayString += generatedMessage[i];
+      arrayString += "-";
+    } else {
+      arrayString += generatedMessage[i].toLowerCase();
+      arrayString += "-";
     }
   }
   return arrayString;
@@ -30,4 +28,4 @@ function arrayToString(generatedMessage) {
 
 let monkeyWords = ["Ooh", "Aah", "Eee", "Ook", "Agh", "Eek"];
 let cowWords = ["Moo", "Moo", "Moo", "Moo", "Moo", "Moo"];
-console.log(arrayToString(generateWordArray(10, cowWords)));
+console.log(arrayToString(generateWordArray(5, monkeyWords)));
